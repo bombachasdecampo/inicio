aad97a34d28682e3168b1d8ce4c65e48cb1132b8
uriel <140184107+bombachasdecampo@users.noreply.github.com>
2023-07-21 14:42:35 -0300

    add the pricing

--- moneyCalculation.js
+++ moneyCalculation.js
@@ -1,3 +1,15 @@
 const dolarValue = document.querySelector(
   `input[type='hidden']:first-of-type`
 ).value;
+
+const prices = document.querySelectorAll(
+  "input[type='hidden']:not(#dolarPrice)"
+);
+
+const outputs = document.querySelectorAll("#prices li  output");
+
+let argentinePesos = [];
+for (index = 0; index < prices.length; index++) {
+  argentinePesos[index] = Number(prices[index].value) * Number(dolarValue);
+  outputs[index].innerText = `${Math.floor(argentinePesos[index])}$ pesos`;
+}
